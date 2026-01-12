f3d8f5dd15975da75965e9e6db73295f0ed5bd1a
Rapteon <rapteon@tutanota.com>
2026-01-12 17:51:46 +0530

    Added "send an email" button in header.

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  
+  private getEmail() {
+    const atTheRate = '@'
+    return 'rapteon' + atTheRate + 'tutanota.com';
+  }
+  emailClicked (event:Event){
+    event.preventDefault();
+    location.href = "mailto:"+this.getEmail()
+  }
+}
